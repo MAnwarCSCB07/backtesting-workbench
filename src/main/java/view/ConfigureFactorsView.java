0211cce479713a7c2805fb478c7aee1cef36600a
mh-anwar <m.anwar@mail.utoronto.ca>
2025-11-25 19:20:33 -0500

    Develop UI to configure basic factors

--- src/main/java/view/ConfigureFactorsView.java
+++ src/main/java/view/ConfigureFactorsView.java
@@ -18,6 +18,43 @@ public class ConfigureFactorsView extends JPanel {
         this.add(title);
 
         this.add(Box.createVerticalStrut(40));
+        JPanel FactorOptionsPanel = new JPanel();
+        FactorOptionsPanel.setLayout(new BoxLayout(FactorOptionsPanel, BoxLayout.Y_AXIS));
+        FactorOptionsPanel.setBorder(BorderFactory.createTitledBorder("Factor Options"));
+
+        JPanel PreprocessingOptionsPanel = new JPanel();
+        PreprocessingOptionsPanel.setLayout(new BoxLayout(PreprocessingOptionsPanel, BoxLayout.Y_AXIS));
+        PreprocessingOptionsPanel.setBorder(BorderFactory.createTitledBorder("Preprocessing Options"));
+
+        // Factor options checkboxes
+        JCheckBox momentumCheck = new JCheckBox("Momentum");
+        JCheckBox valueCheck = new JCheckBox("Value");
+        JCheckBox sizeCheck = new JCheckBox("Size");
+        JCheckBox qualityCheck = new JCheckBox("Quality");
+        JCheckBox volatilityCheck = new JCheckBox("Volatility");
+
+        // Add factor options
+        FactorOptionsPanel.add(momentumCheck);
+        FactorOptionsPanel.add(valueCheck);
+        FactorOptionsPanel.add(sizeCheck);
+        FactorOptionsPanel.add(qualityCheck);
+        FactorOptionsPanel.add(volatilityCheck);
+
+        // Preprocessing options checkboxes
+        JCheckBox winsorizeCheck = new JCheckBox("Winsorize");
+        JCheckBox standardizeCheck = new JCheckBox("Standardize");
+        JCheckBox neutralizeCheck = new JCheckBox("Market Neutralize");
+
+        // Add preprocessing options
+        PreprocessingOptionsPanel.add(winsorizeCheck);
+        PreprocessingOptionsPanel.add(standardizeCheck);
+        PreprocessingOptionsPanel.add(neutralizeCheck);
+
+        // Add panels to main view
+        this.add(FactorOptionsPanel);
+        this.add(Box.createVerticalStrut(20));
+        this.add(PreprocessingOptionsPanel);
+
 
         JButton back = new JButton("Return");
         back.setAlignmentX(Component.CENTER_ALIGNMENT);
