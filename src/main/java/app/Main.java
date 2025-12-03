7469de5893a29026ade08dcb8488ae2c405ceaf3
Aadi Chauhan <aadi104610@gmail.com>
2025-12-02 20:56:56 -0500

    fix: Merge Main.java with new use cases from main branch
    - Add FactorResultsView and RunBacktestView
    - Include RunBacktestUseCase, ImportOHLCVUseCase, FactorConfigUseCase
    - Keep SaveExportUseCase integration
    - Add system look and feel setup from main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,20 +4,31 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Set system look & feel for a more native, polished UI
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {
+        }
+        System.out.println("Main started");
         AppBuilder appBuilder = new AppBuilder();
         JFrame application = appBuilder
-                .addLoginView()
                 .addSignupView()
+                .addLoginView()
                 .addLoggedInView()
                 .addAlphaVantageView()
                 .addInputStockDataView()
                 .addConfigureFactorsView()
+                .addFactorResultsView()
                 .addChartsView()
+                .addRunBacktestView()
                 .addSaveExportView()
                 .addSignupUseCase()
                 .addLoginUseCase()
                 .addLogoutUseCase()
                 .addChangePasswordUseCase()
+                .addRunBacktestUseCase()
+                .addImportOHLCVUseCase()
+                .addFactorConfigUseCase()
                 .addSaveExportUseCase()
                 .build();
 
